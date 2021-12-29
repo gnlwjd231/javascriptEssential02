0a44464e6a217f7e754f6fa98e13a06501e2135f
gnlwjd231 <heee2x1@gmail.com>
2021-12-29 13:20:26 +0900

    프로젝트 생성

---
+++ main.js
@@ -0,0 +1,24 @@
+import getType from './getType'
+
+
+//산술연산자
+
+console.log( 1 + 2 )
+console.log( 1 - 2 )
+console.log( 7 % 5 )
+
+
+//할당 연산자
+
+
+//재할당 불가능
+const a = 2 
+
+//재할당 가능
+let b = 2 
+b += 1
+
+console.log(b)
+
+
+//비교연산자
